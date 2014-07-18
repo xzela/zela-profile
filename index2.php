882f5fc659f412137071070b0dc37bdc4d4f0750
Zeph Lafassett <zeph@lafassett.com>
2014-07-18 12:55:22 -0700

    Adding respond and html5shim

--- index2.php
+++ index2.php
@@ -15,8 +15,8 @@
 
     <!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->
     <!--[if lt IE 9]>
-      <script src="js/html5shiv.js"></script>
-      <script src="js/respond.min.js"></script>
+      <script src="js/third-party/html5shiv/html5shiv.js"></script>
+      <script src="js/third-party/respond/respond.min.js"></script>
     <![endif]-->
   </head>
   <body>
