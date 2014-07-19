b0028b135eda9bceb8ff46b18b6a8c02473a5809
Zeph Lafassett <zeph@lafassett.com>
2014-07-19 13:20:53 -0700

    modifing the styles, adding beta.kink image

--- index2.php
+++ index2.php
@@ -13,6 +13,8 @@
 
     <link rel="stylesheet" href="/css/third-party/font-awesome/font-awesome.min.css">
 
+    <link rel="stylesheet" href="/css/profile.css">
+
     <link rel="shortcut icon" href="images/favicon.ico">
 
     <!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->
@@ -22,45 +24,243 @@
     <![endif]-->
   </head>
   <body>
-    <div class="container">
-<!--         <nav class="navbar header" role="navigation">
+    <nav class="navbar navbar-default" role="navigation">
+        <div class="container-fluid">
             <div class="navbar-header">
-                <a class="navbar-brand" href="/">Me!</a>
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Me!</a>
             </div>
-            <div class="navbar-collapse collapse">
-                <ul class="nav navbar-nav navbar-left">
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
                     <li><a href="#"><span class="fa fa-star"></span> Mad Skillz</a></li>
                     <li><a href="#"><span class="fa fa-flask"></span> Projects</a></li>
-                    <li><a href="#"><span class="fa fa-github"></span> Github</a></li>
+                    <li><a href="https://github.com/xzela"><span class="fa fa-github"></span> Github</a></li>
                     <li><a href="/about.php"><span class="fa fa-info-circle"></span> About</a></li>
-                    <li><a href="#" class="toggle-cloud"><span class="fa fa-cloud"></span> Turn <span class="status">On</span> Cloud</a></li>
                 </ul>
             </div>
-        </nav> -->
-         <nav class="navbar navbar-default" role="navigation">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="#">Me!</a>
+        </div>
+    </nav>
+    <div class="container">
+        <div class="page-header profile-header">
+            <h1 class="">Zeph LaFassett <small> - a kick ass web developer!</small></h1>
+        </div>
+        <div class="container container-project lang">
+            <div class="row">
+                <div class="col-md-6">
+                    <h3>Lang Antiques</h3>
+                    <p class='url'><a href='http://www.langantiques.com' class='external_link'>http://www.langantiques.com</a></p>
+                    <p class='description'>
+                        Zeph designed and implemented a new website for Lang Antiques, an independently-owned jewelry store located in downtown San Francisco.
+                        He also developed a <a class='local' href='#projectmango'> custom CMS</a> for their inventory management needs.
+                        In addition, he also managed their SEO and online marketing, and with his help Lang Antiques enjoys top rankings in Google, Yahoo, and Bing for specific search phrases.
+                        Since improving Lang's workflow and online services, internet traffic and sales have increased over 30%.
+                    </p>
                 </div>
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul class="nav navbar-nav">
-                        <li><a href="#"><span class="fa fa-star"></span> Mad Skillz</a></li>
-                        <li><a href="#"><span class="fa fa-flask"></span> Projects</a></li>
-                        <li><a href="#"><span class="fa fa-github"></span> Github</a></li>
-                        <li><a href="/about.php"><span class="fa fa-info-circle"></span> About</a></li>
-                    </ul>
+                <div class="col-md-6">
+                    <img class="img-responsive" src='images/screenshots/langantiques.png' alt='Lang Antiques screenshot' title='click here to buy some antique jewelry'/>
                 </div>
             </div>
-        </nav>
-        <h1 class="">Zeph LaFassett</h1>
+            <div class="row">
+                <div class="col-md-12">
+                    <p class='duties'>
+                        Lang Antiques uses the PHP framework <a href='http://codeigniter.com' class='external_link'>CodeIgniter</a>.
+                        All frontend development and user experience was coded onsite by Zeph.
+                        He also managed all web related services and servers.
+                    </p>
+                    <h4>tags:</h4>
+                    <hr />
+                    <div>
+                        <span class="label label-default">PHP</span>
+                        <span class="label label-default">Javascript</span>
+                        <span class="label label-default">CodeIgntiter</span>
+                        <span class="label label-default">MySQL</span>
+                        <span class="label label-default">APC</span>
+                        <span class="label label-default">PHP-GD</span>
+                        <span class="label label-default">CSS</span>
+                        <span class="label label-default">SEO</span>
+                        <span class="label label-default">awesome jewelry!</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container container-project">
+            <div class="row">
+                <div class="col-md-6">
+                    <h2>Cardr</h2>
+                    <p class='url'><a href='https://cardr.org/' class='external_link'>https://cardr.org/</a></p>
+                    <p class='description'>
+                        Cardr is a customer safety awareness project.
+                        The general purpose of Cardr is to make users aware of malicous websites that may have alterior motives.
+                    </p>
+                </div>
+                <div class"=col-md-6">
+                    <img class="img-responsive" src='images/screenshots/cardr.png' alt='Cardr - The Credit Card Monitoring Service' title="Don't be fooled" />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <p class='duties'>
+                        Zeph was responsible for general UI design, server setup, ssl setup.
+                    </p>
+                    <h4>tags</h4>
+                    <div>
+                        <span class="label label-default">Node.js</span>
+                        <span class="label label-default">SSL</span>
+                        <span class="label label-default">Grunt</span>
+                        <span class="label label-default">fake site</span>
+                        <span class="label label-default">does not steal credit card information</span>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-        Start doing your next awesomeness here.
+                <div id='projectmango' class='product'>
+                    <h2>Project Mango {</h2>
+                    <div class='screenshot'>
+                        <img src='images/screenshots/project.mango.png' alt='project mango screenshot' />
+                    </div>
+                    <p class='description'>
+                        Project Mango is a custom built solution for Lang Antiques.
+                        Lang Antiques had become disatisified with their existing inventory management system and commissioned Zeph to build a new one.
+                        Project Mango is tightly intergraded with their <a class='local' href='#langantiques'>public facing website,</a> and this helps improve internal workflows and customer relations.
+                    </p>
+                    <p class='duties'>
+                        Project Mango is written with the codeigniter framework and uses a MySQL database.
+                    </p>
+                    <h4>tags</h4>
+                    <ul class="keywords">
+                        <li>php</li>
+                        <li>codeigniter</li>
+                        <li>mysql</li>
+                        <li>CMS</li>
+                        <li>does not takes like a mango</li>
+                    </ul>
+                    <h2>}  <span class='small_text no_js'><a href='#'> //to top</a></span></h2>
+                </div>
+
+                <div id='yonoid' class='product'>
+                    <h2>Yo!Noid {</h2>
+                    <div class='screenshot'>
+                        <img src='images/screenshots/yonoid.png' alt='Yo!Noid screenshot' title='click here to start the fun!' />
+                    </div>
+                    <div class='info'>
+                        <p class='description'>
+                            Yo!Noid is a small experimental web project jointly headed by <a href='http://contolini.net/' class='external_link'>Chris Contolini</a> and Zeph.
+                            The site is an experiment in revealing the amount and types of data a browser collects about the user. In addition, it shows how developers can easily access this information.
+                        </p>
+                        <p class='url'><a href='http://yonoid.lafassett.com' class='external_link'>http://yonoid.lafassett.com</a></p>
+                        <p class='duties'>
+                            Zeph was responsible for all backend development which includes: server setup, mail system, database design, backend business logic and a bit of frontend UX.
+                            The look and feel, images, and UX was contributed by Chris Contolini.
+                        </p>
+                    </div>
+                    <h4>tags</h4>
+                    <ul class="keywords">
+                        <li>php</li>
+                        <li>mysql</li>
+                        <li>javascript</li>
+                    </ul>
+                    <h2>}  <span class='small_text no_js'><a href='#'> //to top</a></span></h2>
+                </div>
+
+                <div id='deitweet' class='product'>
+                    <h2>Deitweet {</h2>
+                    <div class='screenshot'>
+                        <img src='images/screenshots/deitweet.png' alt='Deitweet screenshot' title='click here to watch the feed!'/>
+                    </div>
+                    <p class='description'>
+                        Zeph created <a class='external_link' href='http://deitweet.com'>Deitweet.com</a> (dee-i-tweet) as a knee jerk reaction to cursebird.com.
+                        Deitweet filters for major deity names, similar to the way cursebird pulls tweets with swear words.
+                        Like most of Zeph's side projects, this is simply a fun time waster.
+                        Although this site once featured ads, it made no money.
+                    </p>
+                    <p class='url'><a href='http://deitweet.com' class='external_link'>http://deitweet.com</a></p>
+                    <p class='duties'>
+                        Zeph was responsible for all backend and frontend development.
+                        He utilized the public twitter api to filter and gather each tweet.
+                    </p>
+                    <h4>tags</h4>
+                    <ul class="keywords">
+                        <li>php</li>
+                        <li>twitter api</li>
+                        <li>xhr</li>
+                        <li>had ads</li>
+                        <li>time waster</li>
+                        <li>can be offensive</li>
+                    </ul>
+
+                    <h2>}  <span class='small_text no_js'><a href='#'> //to top</a></span></h2>
+                </div>
+
+                <div id='winadollar' class='product'>
+                    <h2>Win A Dollar! {</h2>
+                    <div class='screenshot'>
+                        <img src='images/screenshots/winadollar.png' alt='Win A Dollar screenshot' title='click here to win a dollar!' />
+                    </div>
+                    <p class='description'>
+                        Win A Dollar is an simple online game in which users try to pick the winning square.
+                        Although this application will not give you a dollar, it's still fun to play a few rounds.
+                    </p>
+                    <p class='url'><a href='http://dollar.lafassett.com' class='external_link'>http://dollar.lafassett.com</a></p>
+                    <p class='duties'>
+                        Zeph was responsible for the database design, UX, frontend and backend development, and that horrible pink background.
+                    </p>
+                    <h4>tags</h4>
+                    <ul class="keywords">
+                        <li>php</li>
+                        <li>mysql</li>
+                        <li>sha256</li>
+                        <li>xhr</li>
+                        <li>time waster</li>
+                        <li>does not actually give out a dollar</li>
+                    </ul>
+                    <h2>}  <span class='small_text no_js'><a href='#'> //to top</a></span></h2>
+                </div>
+
+                <div id='kinkydollars' class='product'>
+                    <h2>Kinky Dollars {</h2>
+                    <div class='screenshot'>
+                        <img src='images/screenshots/kinkydollars.png' alt='project mango screenshot' />
+                    </div>
+                    <p class='description'>Kinky Dollars is an adult entertainment affiliate program for [<span class='nsfw'>nsfw</span>] <a class="external_link" href="http://www.kink.com/">Kink.com</a> </p>
+                    <p class="url">[<span class='nsfw'>nsfw</span>] <a href="http://www.kinkydollars.com">http://www.kinkydollars.com/</a></p>
+                    <p class='duties'>
+                        Zeph rewrote the entire backend for the affiliate ad tools for better performance and site maintainability.
+                        By using the <abbr title="alternitive php caching">APC</abbr> library to help with database load and site performance Zeph was able to reduce database load over 10 fold.
+                        Prior to using a caching solution, Kinkydollars database load rose to epic proportions and would often render the tool inoperable.
+                        Zeph was not responsible for the crummy UI.
+                    </p>
+                    <h4>tags</h4>
+                    <ul class="keywords">
+                        <li>php</li>
+                        <li>mysql</li>
+                        <li>APC</li>
+                        <li>porn</li>
+                    </ul>
+                    <h2>}  <span class='small_text no_js'><a href='#'> //to top</a></span></h2>
+                </div>
+
+                <div id='aboutzeph' class='product'>
+                    <h2>About Zeph {</h2>
+                    <div class='screenshot'>
+                        <img src='images/profile.me.jpg' alt='self' />
+                    </div>
+                    <p>He...</p>
+                    <ul>
+                        <li>loves <a href="http://www.sublimetext.com/2" class='external_link'>sublime text 2</a>.</li>
+                        <li>thinks <a href='http://jquery.com' class='external_link'>jquery</a> is cool.</li>
+                        <li>believes <a href='http://nodejs.org' class='external_link'>nodejs</a> is <span class='misspelling hint' title='Thanks for hovering of this span element. I just wanted to see how many people would actually hover over a dotted underline. By hovering over this span element I have collected your: IP Address, Host Address, DateTime, Session ID, and a few other things. Nothing personal of course, it was just a test.'>teh</span> future.</li>
+                        <li>holds a bachelor of arts in philosophy and religion from <a href='http://www.sfsu.edu/' class='external_link'> sfsu</a>.</li>
+                        <li>wants to raise <a href='http://en.wikipedia.org/wiki/Capybara' class='external_link'>capybaras</a> at some point in the future.</li>
+                        <li>has a <a href='/files/Zeph_LaFassett_Resume.pdf'>resume</a>.</li>
+                    </ul>
+                    <h2>}  <span class='small_text no_js'><a href='#'> //to the top breh</a></span></h2>
+                </div>
     </div>
     <!-- /.container -->
 
