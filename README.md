6a2748d8a193b63225033e2c15c6615454595d9a
Zeph LaFassett <zelafassett@gmail.coml>
2018-01-30 10:26:37 -0800

    [v2] init checkin

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
-zela-profile
-============
+# zela-profile
 
 my personal profile site
+
+
+## Install
+```sh
+npm install
+```
+
+## Build
+```sh
+webpack
+```
+
+## Run
+```sh
+php -S localhost:9000 -t build/
+```
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'build')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'images/',
+                            publicPath: ''
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader"
+                    }
+                ]
+
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            // minify: true,
+            template: path.resolve(__dirname, 'src', 'index.html')
+        }),
+        // new webpack.optimize.UglifyJsPlugin({
+        //     compress: {warnings: false}
+        // })
+    ]
+}
