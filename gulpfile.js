3498e05dd0a3eed328618bdd75fc73e6ee9ca046
Zeph LaFassett <zelafassett@gmail.com>
2020-05-16 14:33:54 -0700

    updating stuff

--- gulpfile.js
+++ gulpfile.js
@@ -3,32 +3,32 @@ const cleanCSS = require('gulp-clean-css');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 
-gulp.task('minify-css', () => {
-	return gulp.src('src/styles.css')
+function css(cb) {
+	gulp.src('src/styles.css')
 		.pipe(cleanCSS({compatibility: 'ie8'}))
 		.pipe(gulp.dest('build'));
-});
+	return cb();
+}
 
-gulp.task('babel-js', () => {
-	return gulp.src('src/**/*.js')
-		.pipe(babel({ presets: ['es2015'] }))
+function js(cb) {
+	gulp.src('src/index.js')
+		.pipe(babel({ presets: ['@babel/preset-env'] }))
 		.pipe(uglify())
 		.pipe(gulp.dest('build'));
-});
+	return cb();
+}
 
-gulp.task('copy-html', () => {
-	return gulp.src('src/index.html')
+function move(cb) {
+	// move index
+	gulp.src('src/index.html')
 		.pipe(gulp.dest('build'));
-});
-
-gulp.task('copy-images', () => {
-	return gulp.src('src/images/*')
+	// move images
+	gulp.src('src/images/*')
 		.pipe(gulp.dest('build/images'));
-});
-
-gulp.task('copy-files', () => {
-	return gulp.src('src/files/*')
+	// move files
+	gulp.src('src/files/*')
 		.pipe(gulp.dest('build/files'));
-});
+	return cb();
+}
 
-gulp.task('default', ['minify-css', 'babel-js', 'copy-html', 'copy-images', 'copy-files']);
+module.exports.default = gulp.series(css, js, move);
